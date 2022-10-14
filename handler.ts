4b1a1eee25bb416865ffe165fa46b8ae08f4c4c4
André Bitencourt <andre.bitencourt@vizir.com.br>
2022-10-13 22:49:07 -0300

    refactor: add response type to handler

--- handler.ts
+++ handler.ts
@@ -1,15 +1,13 @@
-import type { APIGatewayProxyEvent } from "aws-lambda";
+import type { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
-export const handle = async (event: APIGatewayProxyEvent) => {
-  return {
+export const handle = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const response = {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2,
-    ),
+    body: JSON.stringify({
+      message: "Go Serverless v1.0! Your function executed successfully!",
+      input: event,
+    }),
   };
+
+  return response;
 };
