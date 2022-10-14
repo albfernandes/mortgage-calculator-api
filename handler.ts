eb30fe90c7318208567303ef2b317023089fae5e
André Bitencourt <andre.bitencourt@vizir.com.br>
2022-10-14 13:04:31 -0300

    feat: switch to serverless-http

--- handler.ts
+++ handler.ts
@@ -1,28 +1,14 @@
 import type { APIGatewayProxyEvent, Context } from "aws-lambda";
-import { createServer, proxy, Response } from "aws-serverless-express";
-import { Server } from "http";
 import "reflect-metadata";
+import serverless from "serverless-http";
 import { container } from "./src/infrastructure/configurations/container";
 import { ProcessHttpRequest } from "./src/interface/api/process-http-request";
 
 const app = container.get(ProcessHttpRequest).configure();
 
-const lambdaServer = createServer(app);
+const lambdaServer = serverless(app);
 
-const beforeExit = async (lambdaServer: Server): Promise<void> => {
-  lambdaServer.removeAllListeners();
-  lambdaServer.close();
-};
-
-export const handle = async (event: APIGatewayProxyEvent, context: Context): Promise<Response> => {
-  return proxy(lambdaServer, event, context, "PROMISE").promise.then(
-    async (result: Response) => {
-      await beforeExit(lambdaServer);
-      return result;
-    },
-    async (error: Error) => {
-      await beforeExit(lambdaServer);
-      return Promise.reject(error);
-    },
-  );
+export const handle = async (event: APIGatewayProxyEvent, context: Context): Promise<Object> => {
+  const result = await lambdaServer(event, context);
+  return result;
 };
