8563d47096289418e8ecda05ae4d7649ece101f3
André Bitencourt <andre.bitencourt@vizir.com.br>
2022-10-14 12:43:41 -0300

    refactor: close server after responding request

--- handler.ts
+++ handler.ts
@@ -1,14 +1,28 @@
-import "reflect-metadata";
 import type { APIGatewayProxyEvent, Context } from "aws-lambda";
-import awsServerlessExpress from "aws-serverless-express";
-import * as http from "http";
+import { createServer, proxy, Response } from "aws-serverless-express";
+import { Server } from "http";
+import "reflect-metadata";
 import { container } from "./src/infrastructure/configurations/container";
 import { ProcessHttpRequest } from "./src/interface/api/process-http-request";
 
 const app = container.get(ProcessHttpRequest).configure();
 
-const server = awsServerlessExpress.createServer(app);
+const lambdaServer = createServer(app);
 
-export const handle = async (event: APIGatewayProxyEvent, context: Context): Promise<http.Server> => {
-  return awsServerlessExpress.proxy(server, event, context);
+const beforeExit = async (lambdaServer: Server): Promise<void> => {
+  lambdaServer.removeAllListeners();
+  lambdaServer.close();
+};
+
+export const handle = async (event: APIGatewayProxyEvent, context: Context): Promise<Response> => {
+  return proxy(lambdaServer, event, context, "PROMISE").promise.then(
+    async (result: Response) => {
+      await beforeExit(lambdaServer);
+      return result;
+    },
+    async (error: Error) => {
+      await beforeExit(lambdaServer);
+      return Promise.reject(error);
+    },
+  );
 };
